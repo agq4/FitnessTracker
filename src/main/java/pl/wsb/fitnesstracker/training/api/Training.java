11ed1b9fbda552ebcba390089e5e506de9f90cff
agq4 <agnieszka.stanaszek@interia.pl>
2026-03-28 13:37:34 +0100

    Training.java

--- src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
@@ -23,18 +23,38 @@ public class Training {
 
     private double averageSpeed;
 
-    public Training(
-            final User user,
-            final Date startTime,
-            final Date endTime,
-            final ActivityType activityType,
-            final double distance,
-            final double averageSpeed) {
-        this.user = user;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.activityType = activityType;
-        this.distance = distance;
-        this.averageSpeed = averageSpeed;
+    public class Training {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        @ManyToOne
+        @JoinColumn(name = "user_id")
+        private User user;
+        @Column(nullable = false)
+        private Date startTime;
+        private Date endTime;
+        @Column(nullable = false)
+        private ActivityType activityType;
+        private double distance;
+        private double averageSpeed;
+
+        public void setUser(User user) {
+            this.user = user;
+        }
+
+        public Training(
+                final User user,
+                final Date startTime,
+                final Date endTime,
+                final ActivityType activityType,
+                final double distance,
+                final double averageSpeed) {
+            this.user = user;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.activityType = activityType;
+            this.distance = distance;
+            this.averageSpeed = averageSpeed;
+        }
     }
 }
