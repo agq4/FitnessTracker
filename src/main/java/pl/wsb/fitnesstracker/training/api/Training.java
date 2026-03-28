4fa3b436f2610a478c8924d032d097dd53d15445
agq4 <agnieszka.stanaszek@interia.pl>
2026-03-28 13:52:43 +0100

    Training.java

--- src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
@@ -1,60 +1,53 @@
 package pl.wsb.fitnesstracker.training.api;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import pl.wsb.fitnesstracker.training.internal.ActivityType;
 import pl.wsb.fitnesstracker.user.api.User;
 
 import java.util.Date;
 
+
+@Entity
+@Table(name = "trainings")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Training {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
-
+    @Column(nullable = false)
     private Date startTime;
-
     private Date endTime;
-
+    @Column(nullable = false)
     private ActivityType activityType;
-
     private double distance;
-
     private double averageSpeed;
 
-    public class Training {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-        @ManyToOne
-        @JoinColumn(name = "user_id")
-        private User user;
-        @Column(nullable = false)
-        private Date startTime;
-        private Date endTime;
-        @Column(nullable = false)
-        private ActivityType activityType;
-        private double distance;
-        private double averageSpeed;
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-        public void setUser(User user) {
-            this.user = user;
-        }
-
-        public Training(
-                final User user,
-                final Date startTime,
-                final Date endTime,
-                final ActivityType activityType,
-                final double distance,
-                final double averageSpeed) {
-            this.user = user;
-            this.startTime = startTime;
-            this.endTime = endTime;
-            this.activityType = activityType;
-            this.distance = distance;
-            this.averageSpeed = averageSpeed;
-        }
+    public Training(
+            final User user,
+            final Date startTime,
+            final Date endTime,
+            final ActivityType activityType,
+            final double distance,
+            final double averageSpeed) {
+        this.user = user;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.activityType = activityType;
+        this.distance = distance;
+        this.averageSpeed = averageSpeed;
     }
 }
