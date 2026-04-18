ae4ef501b22caff76fbe4270e1e07bdc5d18198c
vdi-terminal <agnieszka.stanaszek@interia.pl>
2026-04-18 13:54:39 +0200

    Encja w Event

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,32 @@
 package pl.wsb.fitnesstracker.event;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.type.descriptor.java.LocalDateTimeJavaType;
+import pl.wsb.fitnesstracker.training.api.Training;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "event")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 // TODO: Define the Event entity with appropriate fields and annotations
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column (nullable = false)
+    private String name;
+    private LocalDate startDate;
+    private String location;
+    public Event (String name, LocalDate startDate, String location) {
+        this.name = name;
+        this.startDate = startDate;
+        this.location = location;
+    }
 }
