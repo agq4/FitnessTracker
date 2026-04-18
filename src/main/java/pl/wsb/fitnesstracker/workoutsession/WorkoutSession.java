5c798f7e9ebb4e9f6897d3c1ed652d46b71f61b3
vdi-terminal <agnieszka.stanaszek@interia.pl>
2026-04-18 13:48:39 +0200

    Encja w workoutsession (poprawa)

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -8,36 +8,38 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.type.descriptor.java.LocalDateTimeJavaType;
 import pl.wsb.fitnesstracker.training.api.Training;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "workout_session")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
+
 
 // TODO: Define the Event entity with appropriate fields and annotations
 public class WorkoutSession {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @ManyToOne
-    @JoinColumn(name = training_id)
+    @JoinColumn(name = "training_id")
     private Training training;
-    private LocalDateTimeJavaType timestamp;
+    private LocalDateTime timestamp;
     private double startLatitude;
     private double startLongitude;
     private double endLatitude;
     private double endLongitude;
     private double altitude;
-    public WorkoutSession (Training training, LocalDateTimeJavaType timestamp, double startLatitude, double startLongitude, double endLatitude, double endLongitude, double altitude)
+
+    public WorkoutSession(Training training, LocalDateTime timestamp, double startLatitude, double startLongitude, double endLatitude, double endLongitude, double altitude) {
         this.training = training;
-    this.timestamp = timestamp;
-    this.startLatitude = startLatitude;
-    this.startLongitude = startLongitude;
-    this.endLatitude = endLatitude;
-    this.endLongitude = endLongitude;
-    this.altitude = altitude;
+        this.timestamp = timestamp;
+        this.startLatitude = startLatitude;
+        this.startLongitude = startLongitude;
+        this.endLatitude = endLatitude;
+        this.endLongitude = endLongitude;
+        this.altitude = altitude;
+    }
 }
-]
